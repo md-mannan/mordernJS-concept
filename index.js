1adeab135754a281518e72c3533dd1661e306d0d
md-mannan <mdmannan580@gmail.com>
2024-05-06 22:45:56 +0300

    3

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+// let Javascript={
+//     name:"Javascript",
+//     libraries:['React','Angular','Vue'],
+//     printLibraries:function(){
+//         console.log(this)
+//         var self=this
+//         this.libraries.forEach(function(a){
+            
+            
+            
+//             console.log(`${self.name} love ${a}`)
+//         })
+//     }
+// }
+
+// Javascript.printLibraries()
+
 let Javascript={
     name:"Javascript",
     libraries:['React','Angular','Vue'],
     printLibraries:function(){
+        console.log(this)
         
-        this.libraries.forEach(function(a){
-            
-            console.log(`${this.name} love ${a}`)
-        })
+        this.libraries.forEach(a=>console.log(`${this.name} love ${a}`))
     }
 }
 
